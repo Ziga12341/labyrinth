4bc02535d79852b864a2adb1c05c5b3d66a7c102
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2022-10-15 16:54:43 +0200

    Function useful_step done. now I can walk around the labyrinth and receive information about:  detected crossroad, location of next valid state or None for hitting the wall

--- main.py
+++ main.py
@@ -97,6 +97,11 @@ class Labyrinth:
         # L=Left | R=Right | D=Down | U=Up (Primer: DDRULLGDR)
         self.directions = {"L": (-1, 0), "R": (1, 0), "D": (0, 1), "U": (0, -1) }
         self.initial_step = self.start()
+        self.all_crossroads_absolute = self.all_crossroads()
+        self.first_position = self.start()
+        self.last_position = self.finish()
+        self.two_connected_crossroads = set()  # two connected crossroads dead crossroads in between
+        self.current_branch = []
         self.path = [self.initial_step, (2,2)]
         self.intersections = []
 
@@ -137,13 +142,77 @@ class Labyrinth:
     def next_step(self, previous_step, direction):
         x0, y0 = previous_step
         x1, y1 = self.directions[direction]
-
         return x0 + x1, y0 + y1
+
+    def is_not_wall(self, location):
+        return self.get_any_points_symbol(*location) == " "
+
+    # write in one line...
+    # duplicate of self.point_three_possible_step()
+    def step_all_directions(self, your_location):
+        all_possible_next_steps = set()
+        for direction in self.directions:
+            if self.is_not_wall(self.next_step(your_location, direction)):
+                all_possible_next_steps.add(self.next_step(your_location, direction))
+        return all_possible_next_steps
+
+    # Function return tuple for detected crossroad, location of next valid state or None for hitting the wall
+    def useful_step(self, your_location, crossroad_location):
+        next_step = list(self.step_all_directions(your_location))
+        for possible_steps in next_step:
+            if possible_steps == crossroad_location:
+                next_step.remove(crossroad_location)
+        if len(next_step) > 1:
+            print("We reached Crossroad, Crossroad location is: ", your_location)
+            return your_location, next_step
+        elif len(next_step) == 1:
+            return next_step[0]
+        else:
+            print("We hit the wall, blind road detected")
+            return None
+
+    def next_valid_step(self, possible_ways):
+        self.current_branch.append(possible_ways)
+        for direction in self.directions:
+            next_step = self.next_step(self.current_branch[-1], direction)
+            if self.is_not_wall(next_step) or next_step in self.current_branch:
+                self.current_branch.append(next_step)
+            else:
+                return self.current_branch[0]
+
+    # function return next step (coordinate) is there is only one option where to go, if hit the wall return None
+    def step_by_step(self, previous_location, crossroad):
+        for direction in self.directions:
+            next_step = self.next_step(previous_location, direction)
+            if self.is_not_wall(next_step) and next_step != crossroad:
+                if next_step in self.all_crossroads_absolute:   # optimise this part of code it takes 5 seconds to get data
+                    print("We hit another crossroad")
+                else:
+                    return next_step
+            else:
+                print("I hit the wall")
+
+    def branch_path(self, crossroad):
+        self.current_branch.clear()
+        self.current_branch.append(crossroad)
+        possible_directions = set(self.point_three_possible_step(crossroad))
+        for possible_direction in possible_directions:
+            self.step_by_step(possible_direction, crossroad)
+
 
     # is specified direction free
     def is_free(self, direction):
         location = self.next_step(self.path[-1], direction)
         return self.get_any_points_symbol(*location) == " "
+
+    # return start of one way dead roads
+    def dead_road_start_coordinates(self, start, crossroad):
+        where_i_come_from = crossroad
+        #stop if you hit crossroad or wall
+        while ...:
+            for direction in self.directions:
+                next_step = self.next_step(where_i_come_from, direction)
+        # pojdi naprej, če ni stene in če nisi iz tam prišel
 
     def one_way(self, previous_location):
         possible_steps = []
@@ -157,9 +226,6 @@ class Labyrinth:
             self.intersections.extend(possible_steps)
         print(self.path)
         print(self.intersections)
-
-    def is_not_wall(self, location):
-        return self.get_any_points_symbol(*location) == " "
 
     def point_three_possible_step(self, point):
         three_ways = set()
@@ -181,7 +247,7 @@ class Labyrinth:
 
     def all_crossroads(self):
         # add start and finish to all crossroads
-        three_directions = {(1, 0), (40, 79)}
+        three_directions = set()  # {(1, 0), (40, 79)}
         for point in self.all_possible_points():
             if self.is_not_wall(point) and self.crossroad(point):
                 three_directions.add(point)
@@ -201,24 +267,26 @@ class Labyrinth:
         # če sta dva povezljiva imaš kačo, verigo in izločiš še eno pot med njima
         # če se dva križišča lahko povežeta je to sigurno edina pot, pomeni da imaš daljši kačo
 
-    # def crossroads_with_two_blind_alleys(self, crossroad=(7,1)):
-    #     possible_directions = set(self.point_three_possible_step(crossroad))
-    #     for possible_direction in possible_directions:
-    #         # where you can go from possible direction
-    #
-    #     return possible_directions
 
-    # function return next step is there is only one option where to go, if hit the wall return None
-    def step_by_step(self, previous_location, crossroad):
-        for direction in self.directions:
-            next_step = self.next_step(previous_location, direction)
-            if self.is_not_wall(next_step) and next_step != crossroad:
-                if next_step in self.all_crossroads():   # optimise this part of code
-                    print("We hit another crossroad")
-                else:
-                    return next_step
-            else:
-                print("I hit the wall")
+    def one_branch(self, previous_location, crossroad):
+        self.current_branch
+        ...
+
+
+    def crossroads_with_two_blind_alleys(self, crossroad=(7,1)):
+        possible_directions = set(self.point_three_possible_step(crossroad))
+        for possible_direction in possible_directions:
+            self.step_by_step(possible_direction, crossroad)
+
+        return possible_directions
+
+    # 1. hit the wall
+    # 2. hit the crossroad
+    # 3. hit self.start()
+    # 4. hit self.finish()
+    # goal is to remove dead crossroads from all_crossroads
+    # and to connect alive crossroads
+
 
     def connect_two_crossroads(self):
         ...
@@ -227,8 +295,8 @@ labyrinth = Labyrinth()
 # print(labyrinth.open_file())
 print(labyrinth.labyrinth_size_x())
 print(labyrinth.labyrinth_size_y())
-print(labyrinth.start())
-print(labyrinth.finish())
+print(labyrinth.first_position)
+print(labyrinth.last_position)
 print(labyrinth.get_any_points_symbol(1, 1))
 print(labyrinth.next_step((1, 0), "D"))
 print("---------------")
@@ -246,12 +314,15 @@ print(labyrinth.get_any_points_symbol(80, 40))
 
 print("---------------")
 
-print("All possible crossroads are: ", labyrinth.all_crossroads())
-print("No of all possible crossroads are: ", len(labyrinth.all_crossroads()))
+print("All possible crossroads are: ", labyrinth.all_crossroads_absolute)
+print("No of all possible crossroads are: ", len(labyrinth.all_crossroads_absolute))
 
 # print("Two blind streets", labyrinth.crossroads_with_two_blind_alleys())
 
 print(labyrinth.step_by_step((6, 1), (7, 1)))
+print(labyrinth.step_all_directions((9, 1)))
+print("Useful step, next valid step is: ", labyrinth.useful_step((9, 1), (8, 1)))
+
 
 """
 print(labyrinth)
